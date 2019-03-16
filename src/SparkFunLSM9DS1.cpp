e3921b4d9d9a580e9123a904b49776445cefe617
Tomáš Polišenský <kammtar@gmail.com>
2019-03-16 02:12:32 +0100

    bug fix: temp. sensor sensitivity is 16 LSB/C. Which means we want to do bit shift by 4 not 8 to divide by 16.

--- src/SparkFunLSM9DS1.cpp
+++ src/SparkFunLSM9DS1.cpp
@@ -573,7 +573,7 @@ void LSM9DS1::readTemp()
 	if ( xgReadBytes(OUT_TEMP_L, temp, 2) == 2 ) // Read 2 bytes, beginning at OUT_TEMP_L
 	{
 		int16_t offset = 25;  // Per datasheet sensor outputs 0 typically @ 25 degrees centigrade
-		temperature = offset + ((((int16_t)temp[1] << 8) | temp[0]) >> 8) ;
+		temperature = offset + ( (((int16_t)temp[1] << 8) | (int16_t)temp[0]) >> 4 );
 	}
 }
 
